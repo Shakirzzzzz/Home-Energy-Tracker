cc11ab09deab5864ecfe602c641d112ecddc60d7
Shakir <s.shakirz26@gmail.com>
2026-06-09 21:18:39 +0530

    Implements OpenAPI docs for the Api Gateway

---
+++ ingestion-service/src/main/java/com/shuku/ingestion_service/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package com.shuku.ingestion_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI ingestionServiceApiDocs(){
+
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Ingestion Service API")
+                        .contact(getContact())
+                        .license(getLicense())
+                        .description("Ingestion service API for Home Energy Tracker")
+                        .version("1.0.0"));
+
+    }
+
+    private static License getLicense(){
+        License license = new License();
+        license.setName("Creative Commons Attributions-NonCommercial 4.0 International License");
+        license.setUrl("http://creativecommons.org/licenses/by-nc/4.0/");
+        return license;
+    }
+
+    private static Contact getContact(){
+        Contact contact = new Contact();
+        contact.setEmail("s.shakirz26@gmail.com");
+        return contact;
+    }
+}
--- ingestion-service/src/main/java/com/shuku/ingestion_service/controller/IngestionController.java
+++ ingestion-service/src/main/java/com/shuku/ingestion_service/controller/IngestionController.java
@@ -2,11 +2,14 @@ package com.shuku.ingestion_service.controller;
 
 import com.shuku.ingestion_service.dto.EnergyUsageDto;
 import com.shuku.ingestion_service.service.IngestionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/ingestion")
+@Tag(name= "Ingestion Endpoint", description = "Endpoint to ingest data of an IOT Device")
 public class IngestionController {
 
     private  final IngestionService ingestionService;
@@ -17,6 +20,7 @@ public class IngestionController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Takes data from an IOT device", description = "This endpoint can be used to ingest data of an IOT device for a user")
     public void ingestData(@RequestBody EnergyUsageDto usageDto){
         ingestionService.ingestEnergyUsage(usageDto);
     }
